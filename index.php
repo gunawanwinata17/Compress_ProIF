8b51fd3b6a23921a487257385e60bf9c22ccf190
gunawanwinata17 <96789079+gunawanwinata17@users.noreply.github.com>
2024-09-09 17:15:34 +0700

    Buat upload PHP

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Video Compressor
-        </title>
+        <title>Video Compressor</title>
     </head>
     <body>
         <?php echo '<p>Compress Video Here</p>'; ?>
+        
+        <!-- Form untuk upload file video -->
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+            <label for="videoUpload">Upload Video (.mp4):</label>
+            <input type="file" name="video" id="videoUpload" accept=".mp4" required>
+            <br><br>
+            <input type="submit" value="Upload Video">
+        </form>
     </body>
-</html
+</html>
