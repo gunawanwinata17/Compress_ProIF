08f9d6f492495531a7f3287bf62ef2c423a9a9a5
yohanes <yohanessitorusp@gmail.com>
2024-09-11 17:32:03 +0700

    debug fitur upload file

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
+<?php 
+
+//target direktori file yg diupload pada server 
+$targetDir = "uploads/" ;
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    //mengambil detail dari file yg diupload
+    $file = $_FILES['video'];
+    $targetFile = $targetDir . basename($file['name']) ;
+
+    //periksa apakah file yg diupload dalam format video MP4
+    $fileType = mime_content_type($file['tmp_name']) ;
+    $allowedType = 'vdeo/mp4' ;
+
+    if($fileType !== $allowedType) {
+        die("Only .mp4 video files are allowed.") ;
+    }
+
+    //periksa apakah terjadi error saat upload video
+    if($file['error'] !== UPLOAD_ERR_OK) {
+        die("Error occurred while uploading the file.") ;
+    }
+
+    //pindahkan file yg diupload ke direktori target
+    if(move_uploaded_file($file['tmp_name'], $targetFile)) {
+        echo "The file " . htmlspecialchars($file['name']) . " has been uploaded successfully." ;
+    }else{
+        echo "Sorry there was an error uploading your file." ;
+    }
+
+}
+
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -62,7 +96,7 @@
             <?php echo '<h1>Compress Video Here</h1>'; ?>
             
             <!-- form untuk upload file video -->
-            <form action="upload.php" method="post" enctype="multipart/form-data">
+            <form action="index.php" method="post" enctype="multipart/form-data">
                 <label for="videoUpload">Upload Video (.mp4):</label>
                 <input type="file" name="video" id="videoUpload" accept=".mp4" required>
                 <br>
