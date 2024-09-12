3988e953d3446bbc229e44f6f1615e73d779b080
Rafi R <rafirahman30@icloud.com>
2024-09-12 23:32:05 +0700

    menambahkan button command untuk uji coba perintah ls yang akan ditampilkan di web namun belum berhasil

--- run.php
+++ run.php
@@ -1,11 +1,31 @@
 <?php
+function runCompress($file)
+{
+    $host = 'compress.ifunpar.id';
+    $user = 'gunawan';
+    $password = 'UNPAR123';
+    $remote_directory = '/home/gunawan/uploads/' . basename($file);
+    $local_file = $file;
+    
+    // Perintah SSH dengan password
+    $scp_command = "sshpass -p '$password' scp $local_file $user@$host:$remote_directory";
+    
+    // Jalankan perintah untuk meng-upload file ke server remote
+    exec($scp_command, $output, $return_var);
+    if ($return_var === 0) {
+        echo "File uploaded to remote server successfully.<br>";
 
-$file = $_FILES['video'];
-$ssh = "ssh gunawan@compress.ifunpar.id";
-$password = "UNPAR123"; //harus disetting lagi agar aman
-//$compressedFile
-
-//encode h.265
-//$runCompress = shell_exec("ffmpeg -i $file -vcodec libx265" $compressedFile)
-
+        // Jalankan ffmpeg di server remote untuk kompresi
+        $ffmpeg_command = "sshpass -p '$password' ssh $user@$host 'ffmpeg -i $remote_directory -vcodec libx265 /home/gunawan/uploads/compressed_" . basename($file) . "'";
+        exec($ffmpeg_command, $output, $return_var);
+        
+        if ($return_var === 0) {
+            echo "Compression successful.<br>";
+        } else {
+            echo "Compression failed.<br>";
+        }
+    } else {
+        echo "Failed to upload file to remote server.<br>";
+    }
+}
 ?>
