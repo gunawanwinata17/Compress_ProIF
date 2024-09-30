991175a075ebed124fa2254432e8da011d05386b
yohanes <yohanessitorusp@gmail.com>
2024-09-30 09:26:53 +0700

    combining process.php in rahman branch with main branch

--- process.php
+++ process.php
@@ -16,6 +16,25 @@ $result = $conn->query($sql);
 if($result->num.rows > 0) {
     while($row = $result->fetch_assoc()) {
 
+        $fileName = $row['fileName'];
+
+        //inisialisasi file mentahan dan nama output file setelah kompress berhasil
+        $rawFile = 'uploads/' . $fileName;
+        $compressedFile = 'uploads/compressed_' . $fileName;
+        $ffmpegCommand = "ffmpeg -i " . escapeshellarg($rawFile) . " -c:v libx264 -crf 23 -preset medium -c:a aac -b:a 128k " . escapeshellarg($compressedFile);
+        exec($ffmpegCommand, $output, $check);
+
+        if ($check === 0)
+            $query = "update db set status = 1 where fileName = ?";//berhasil
+        else
+            $query = "upadate db set status = -1 where fileName = ?";//gagal
+        
+            //exec query nya
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param("s", $fileName);
+        $stmt->execute();
+        $stmt->close();
+
     }
 }else{
     echo "no processing file"
