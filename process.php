0a3e7fc85767af90b89716701c1b56aadb22ea80
yohanes <yohanessitorusp@gmail.com>
2024-10-10 14:22:03 +0700

    bug fixing

--- process.php
+++ process.php
@@ -17,20 +17,23 @@ if($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
 
         $fileName = $row['fileName'];
+        $fileID = $row['id'];
 
         //inisialisasi file mentahan dan nama output file setelah kompress berhasil
         $rawFile = 'uploads/' . $fileName;
-        $compressedFile = 'uploads/compressed_' . $fileName;
+        $compressedFile = 'uploads/compressed_' . $fileName . $fileID;
         $ffmpegCommand = "ffmpeg -i " . escapeshellarg($rawFile) .  " -c:v libx264 -crf 23 -preset medium -c:a aac -b:a 128k " . escapeshellarg($compressedFile);
         
         ob_start();
         system($ffmpegCommand, $returnCode);
+        $output = ob_get_contents() ;
         ob_end_clean();
 
         if ($returnCode === 0)
             $query = "update db set status = 1 where fileName = ?";//berhasil
         else
             $query = "update db set status = -1 where fileName = ?";//gagal
+            echo "Error: " . $output ;
         
             //exec query nya
         $stmt = $conn->prepare($query);
