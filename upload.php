c5e07b6ed4567d3825e39ab78cd3082662438b9a
yohanes <yohanessitorusp@gmail.com>
2024-10-12 09:40:52 +0700

    bug fixing

--- upload.php
+++ upload.php
@@ -5,7 +5,7 @@ require('db.php');
 $targetDir = "uploads/";
 
 if (!is_dir($targetDir)) {
-    mkdir($targetDir, 0775, true);
+    mkdir($targetDir, 0777, true);
 }
 
 //mengambil detail dari file yang diupload
