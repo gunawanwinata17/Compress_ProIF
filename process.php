7e55b18b5b3cbb339ce25ea178673810649d9c15
yohanes <yohanessitorusp@gmail.com>
2024-10-14 08:42:59 +0700

    bug fixing

--- process.php
+++ process.php
@@ -27,10 +27,9 @@ if($result->num_rows > 0) {
         $stmtUpdate->close();
 
         // Inisialisasi file mentahan dan nama output file setelah kompres berhasil
-        $rawFile = '/home/gunawan/proif/Compress_ProIF/uploads/' . $fileName;
-        $compressedFile = '/home/gunawan/proif/Compress_ProIF/uploads/compressed_' . $fileID . '_' . $fileName;
-        $ffmpegPath = '/usr/bin/ffmpeg';
-        $$ffmpegCommand = $ffmpegPath . " -i "  . escapeshellarg($rawFile) .  " -c:v libx264 -crf 23 -preset medium -c:a aac -b:a 128k " . escapeshellarg($compressedFile);
+        $rawFile = $fileName;
+        $compressedFile = 'compressed_' . $fileID . '_' . $fileName;
+        $$ffmpegCommand = "ffmpeg -i "  . escapeshellarg($rawFile) .  " -c:v libx264 -crf 23 -preset medium -c:a aac -b:a 128k " . escapeshellarg($compressedFile);
 
         ob_start();
         system("$ffmpegCommand 2>&1", $returnCode); 
